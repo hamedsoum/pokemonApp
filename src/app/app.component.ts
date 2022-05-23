022377ca44189c5df42874fe75dcc0a089760e5f
hamedsoum <hamedsoum4023@gmail.com>
2022-05-23 09:42:54 +0000

    okemon detail page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent implements OnInit {
-  pokemonList :Pokemon[] = POKEMONS;
-  pokemonSelected : Pokemon|undefined;
-
-  ngOnInit(): void {
-      console.table(this.pokemonList);    
-  }
-
-  selectPokemon(pokemonId : String){
-    const pokemon : Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-    if (pokemon) {
-      console.log(`vous avez démandez sur le pokemon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    }else{
-      console.log(`vous avez démandez un pokemon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-  }
+export class AppComponent {
 }
